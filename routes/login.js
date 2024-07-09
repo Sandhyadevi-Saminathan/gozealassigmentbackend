44eba42970b1325c0f59e6ea249d4ff34874a71b
Sandhya devi Saminathan <sandhya021999@gmail.com>
2024-07-09 12:04:25 +0530

    commit

--- routes/login.js
+++ routes/login.js
@@ -8,38 +8,36 @@ const secret = process.env.SECRET;
 
 router.post('/', async (req, res) => {
     try {
+        // Normalize the email to lowercase
+        const normalizedEmail = req.body.email.toLowerCase();
+console.log(normalizedEmail)
         let connection = await mongoClient.connect(URL);
         let db = connection.db('project');
         const collection = db.collection("register");
-        const user = await collection.findOne({ email: req.body.email });
+        const user = await collection.findOne({ email: normalizedEmail });
 
         if (user) {
             let passwordResult = await bcrypt.compare(req.body.password, user.password);
             if (passwordResult) {
-                const token = jwt.sign({ userid: user._id }, secret, { expiresIn: '1h' })
-                
-                res.json({ message: "Login Success", token, user })
-
-            }
-            else {
-                res.status(401).json({ message: "Email id or password do not match" })
+                const token = jwt.sign({ userid: user._id }, secret, { expiresIn: '1h' });
+                res.json({ message: "Login Success", token, user });
+            } else {
+                res.status(401).json({ message: "Email id or password do not match" });
             }
         } else {
-            res.status(401).json({ message: "Email id or password donot match" });
+            res.status(401).json({ message: "Email id or password do not match" });
         }
     } catch (error) {
         console.error("Error during login:", error);
         const errorPayload = {
             message: "Server error",
             originalMessage: error.message,
-            originalStatus: error.status || 500, // Assuming status is a custom field you set
+            originalStatus: error.status || 500,
         };
-
         res.status(500).json(errorPayload);
-        // Handle server error
-       
-    } 
-})
+    }
+});
+
 
 
 
